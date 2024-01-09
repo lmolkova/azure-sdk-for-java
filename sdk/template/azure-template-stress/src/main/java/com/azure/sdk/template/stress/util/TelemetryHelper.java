3389ea63a118e54904c1baeca6745410137a2680
Liudmila Molkova <limolkova@microsoft.com>
2024-01-08 20:01:03 -0800

    telemtery and jre

--- sdk/template/azure-template-stress/src/main/java/com/azure/sdk/template/stress/util/TelemetryHelper.java
+++ sdk/template/azure-template-stress/src/main/java/com/azure/sdk/template/stress/util/TelemetryHelper.java
@@ -5,21 +5,36 @@ package com.azure.sdk.template.stress.util;
 
 import com.azure.core.http.HttpClientProvider;
 import com.azure.core.util.logging.ClientLogger;
+import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 import com.azure.sdk.template.stress.StressOptions;
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.runtimemetrics.java8.Classes;
+import io.opentelemetry.instrumentation.runtimemetrics.java8.Cpu;
+import io.opentelemetry.instrumentation.runtimemetrics.java8.GarbageCollector;
+import io.opentelemetry.instrumentation.runtimemetrics.java8.MemoryPools;
+import io.opentelemetry.instrumentation.runtimemetrics.java8.Threads;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
+import io.opentelemetry.sdk.trace.data.LinkData;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -30,6 +45,7 @@ public class TelemetryHelper {
     private final ClientLogger logger;
     private static final AttributeKey<String> SCENARIO_NAME_ATTRIBUTE = AttributeKey.stringKey("scenario_name");
     private static final AttributeKey<String> ERROR_TYPE_ATTRIBUTE = AttributeKey.stringKey("error.type");
+    private static final AttributeKey<Boolean> SAMPLE_IN_ATTRIBUTE = AttributeKey.booleanKey("sample.in");
     private final String scenarioName;
     private final Meter meter;
     private final DoubleHistogram runDuration;
@@ -41,8 +57,41 @@ public class TelemetryHelper {
         Schedulers.enableMetrics();
     }
 
+
+    public static void init() {
+        AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
+        new AzureMonitorExporterBuilder()
+                .connectionString(System.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"))
+                .install(sdkBuilder);
+
+        OpenTelemetry otel = sdkBuilder
+                .addSamplerCustomizer((sampler, props) -> new Sampler() {
+                    @Override
+                    public SamplingResult shouldSample(Context parentContext, String traceId, String name, SpanKind spanKind, Attributes attributes, List<LinkData> parentLinks) {
+                        if (Boolean.TRUE.equals(attributes.get(SAMPLE_IN_ATTRIBUTE))) {
+                            return SamplingResult.recordAndSample();
+                        }
+                        return sampler.shouldSample(parentContext, traceId, name, spanKind, attributes, parentLinks);
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return sampler.getDescription();
+                    }
+                })
+                .setResultAsGlobal()
+                .build()
+                .getOpenTelemetrySdk();
+        Classes.registerObservers(otel);
+        Cpu.registerObservers(otel);
+        MemoryPools.registerObservers(otel);
+        Threads.registerObservers(otel);
+        GarbageCollector.registerObservers(otel);
+    }
+
     /**
      * Creates an instance of telemetry helper.
+     *
      * @param scenarioClass the scenario class
      */
     public TelemetryHelper(Class<?> scenarioClass) {
@@ -51,14 +100,15 @@ public class TelemetryHelper {
         this.meter = GlobalOpenTelemetry.getMeter(scenarioName);
         this.logger = new ClientLogger(scenarioName);
         this.runDuration = meter.histogramBuilder("test.run.duration")
-            .setUnit("s")
-            .build();
+                .setUnit("s")
+                .build();
         this.commonAttributes = Attributes.of(SCENARIO_NAME_ATTRIBUTE, scenarioName);
         this.canceledAttributes = Attributes.of(SCENARIO_NAME_ATTRIBUTE, scenarioName, ERROR_TYPE_ATTRIBUTE, "cancelled");
     }
 
     /**
      * Instruments a runnable: records runnable duration along with the status (success, error, cancellation),
+     *
      * @param oneRun the runnable to instrument
      */
     @SuppressWarnings("try")
@@ -79,6 +129,7 @@ public class TelemetryHelper {
 
     /**
      * Instruments a Mono: records mono duration along with the status (success, error, cancellation),
+     *
      * @param runAsync the mono to instrument
      * @return the instrumented mono
      */
@@ -89,16 +140,16 @@ public class TelemetryHelper {
             Span span = tracer.spanBuilder("runAsync").startSpan();
             try (Scope s = span.makeCurrent()) {
                 return runAsync.doOnError(e -> trackFailure(start, e, span))
-                    .doOnCancel(() -> trackCancellation(start, span))
-                    .doOnSuccess(v -> trackSuccess(start, span))
-                    .contextWrite(reactor.util.context.Context.of(com.azure.core.util.tracing.Tracer.PARENT_TRACE_CONTEXT_KEY, io.opentelemetry.context.Context.current()));
+                        .doOnCancel(() -> trackCancellation(start, span))
+                        .doOnSuccess(v -> trackSuccess(start, span))
+                        .contextWrite(reactor.util.context.Context.of(com.azure.core.util.tracing.Tracer.PARENT_TRACE_CONTEXT_KEY, io.opentelemetry.context.Context.current()));
             }
         });
     }
 
     private void trackSuccess(Instant start, Span span) {
         logger.atInfo()
-            .log("run ended");
+                .log("run ended");
 
         runDuration.record(getDuration(start), commonAttributes);
         span.end();
@@ -106,8 +157,8 @@ public class TelemetryHelper {
 
     private void trackCancellation(Instant start, Span span) {
         logger.atWarning()
-            .addKeyValue("error.type", "cancelled")
-            .log("run ended");
+                .addKeyValue("error.type", "cancelled")
+                .log("run ended");
 
         runDuration.record(getDuration(start), canceledAttributes);
         span.setAttribute(ERROR_TYPE_ATTRIBUTE, "cancelled");
@@ -123,8 +174,8 @@ public class TelemetryHelper {
 
         String errorType = unwrapped.getClass().getName();
         logger.atError()
-            .addKeyValue("error.type", errorType)
-            .log("run ended", unwrapped);
+                .addKeyValue("error.type", errorType)
+                .log("run ended", unwrapped);
 
         Attributes attributes = Attributes.of(SCENARIO_NAME_ATTRIBUTE, scenarioName, ERROR_TYPE_ATTRIBUTE, errorType);
         runDuration.record(getDuration(start), attributes);
@@ -133,6 +184,7 @@ public class TelemetryHelper {
 
     /**
      * Records an event representing the start of a test along with test options.
+     *
      * @param options test parameters
      */
     public void recordStart(StressOptions options) {
@@ -145,8 +197,8 @@ public class TelemetryHelper {
             }
         } catch (ClassNotFoundException e) {
             logger.atWarning()
-                .addKeyValue("class", HttpClientProvider.class.getName())
-                .log("could not find class", e);
+                    .addKeyValue("class", HttpClientProvider.class.getName())
+                    .log("could not find class", e);
         }
 
         Span before = startSampledInSpan("before run");
@@ -159,11 +211,14 @@ public class TelemetryHelper {
         before.setAttribute(AttributeKey.stringKey("hostname"), System.getenv().get("HOSTNAME"));
         before.setAttribute(AttributeKey.stringKey("serviceEndpoint"), options.getServiceEndpoint());
         before.setAttribute(AttributeKey.stringKey("httpClientProvider"), options.getHttpClient().toString());
+        before.setAttribute(AttributeKey.stringKey("jreVersion"), System.getProperty("java.version"));
+        before.setAttribute(AttributeKey.stringKey("jreVendor"), System.getProperty("java.vendor"));
         before.end();
     }
 
     /**
      * Records an event representing the end of the test.
+     *
      * @param startTime the start time of the test
      */
     public void recordEnd(Instant startTime) {
@@ -175,10 +230,10 @@ public class TelemetryHelper {
 
     private Span startSampledInSpan(String name) {
         return tracer.spanBuilder(name)
-            // guarantee that we have before/after spans sampled in
-            // and record duration/result of the test
-            .setAttribute("sample.in", "true")
-            .startSpan();
+                // guarantee that we have before/after spans sampled in
+                // and record duration/result of the test
+                .setAttribute(SAMPLE_IN_ATTRIBUTE, true)
+                .startSpan();
     }
 
     private static double getDuration(Instant start) {
